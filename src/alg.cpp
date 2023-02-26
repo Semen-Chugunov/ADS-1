9c26bbe18de9f17b7ecd3b27288b251302a37ef0
Semen-Chugunov <125124591+Semen-Chugunov@users.noreply.github.com>
2023-02-26 03:46:54 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -13,30 +13,30 @@ bool checkPrime(uint64_t value) {
 }
 
 uint64_t nPrime(uint64_t n) {
-  uint64_t t = 0;
-  for (uint64_t i = 0 ; i < n; i++) {
-    if (checkPrime(i))
-      t = i;
-  }
-  return t;
+    uint64_t t = 0;
+    for (uint64_t i = 0 ; i < n; i++) {
+        if (checkPrime(i))
+            t = i;
+        }
+    return t;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  uint64_t n = 1;
-  while (1) {
+    uint64_t n = 1;
+    while (1) {
     if (checkPrime(value + n)) {
-      return value + n;
-      break;
+        return value + n;
+        break;
     }
-  n++;
-  }
+    n++;
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum = 0;
-  for (uint64_t i = 0; i < hbound; i++) {
-    if (checkPrime(i))
-      sum += i;
-  }
-  return sum;
+    uint64_t sum = 0;
+    for (uint64_t i = 0; i < hbound; i++) {
+        if (checkPrime(i))
+        sum += i;
+    }
+    return sum;
 }
